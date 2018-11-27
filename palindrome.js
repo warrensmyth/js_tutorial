89e872550ec3e19a18215286ed0132973ea328c5
Warren Smyth <warrensmyth@me.com>
2018-11-27 14:22:19 -0800

    Make palindrome ignore typecase

--- palindrome.js
+++ palindrome.js
@@ -3,6 +3,8 @@ function reverse(string) {
 	return string.split("").reverse().join("");
 }
 
+// Returns true for a palindrome, false otherwise.
 function palindrome(string) {
-	return string === reverse(string);
+	let processedContent = string.toLowerCase();
+	return processedContent === reverse(processedContent);
 }
