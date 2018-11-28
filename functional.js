f3630fd1792c5f61945731aa7b19fc3d14eb837f
Warren Smyth <warrensmyth@me.com>
2018-11-27 17:08:51 -0800

    6.2 Excercise 1 part 1

--- functional.js
+++ functional.js
@@ -22,8 +22,35 @@ function functionalUrls(elements) {
 }
 console.log(functionalUrls(states))
 
-// Exercise 1
+// Section 6.1, Exercise 1
 function fullUrls(elements) {
 	return elements.map(element => ("https://example.com/" + urlify(element)))
 }
 console.log(fullUrls(states))
+
+// singles: Imperative version
+function imperativeSingles(elements) {
+	let singles = [];
+	elements.forEach(function(element) {
+		if (element.split(/\s+/).length === 1) {
+			singles.push(element);
+		}
+	});
+	return singles;
+}
+console.log(imperativeSingles(states));
+
+// singles: Functional version
+function functionalSingles(elements) {
+	return elements.filter(element => element.split(/\s+/).length === 1);
+}
+console.log(functionalSingles(states));
+
+// Section 6.2, Exercise 1 part 1
+function stringDakota(elements) {
+	return elements.filter(element => element.includes("Dakota"));
+}
+console.log(stringDakota(states));
+
+// Section 6.2 Excercise 1 part 2
+function 
